db1d02b843193d563bc33ad41d2d82a0bcb6d573
Mudacumura Brunoblaise <25270994+brunoblaise@users.noreply.github.com>
2021-08-09 16:36:14 +0200

    as

--- src/land/Land.jsx
+++ src/land/Land.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import Group5 from '../images/Group5.svg'
-import Group1 from '../images/Group1.png'
-import Group12 from '../images/Group12.svg'
+
 import Group171 from '../images/Group171.svg'
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import 'react-lazy-load-image-component/src/effects/blur.css';
 function Land() {
     return (
         <>
-        <header id="header-section">
+        <header className="header-section">
         <nav className="navbar navbar-expand-lg pl-3 pl-sm-0" id="navbar">
             <div className="container">
                 <div className="navbar-brand-wrapper d-flex w-100">
@@ -56,58 +54,9 @@ function Land() {
             <LazyLoadImage effect="blur" src={Group171} alt=""width="640" height="360" className="img-fluid"/>
         </div>
     </div>
-    <div className="content-wrapper">
-        <div className="container">
-            <section className="features-overview" id="features-section">
-                <div className="content-header">
-                    <h2>How does it works</h2>
-                    <h6 className="section-subtitle text-muted">One theme that serves as an easy-to-use operational toolkit<br/>that meets students's needs.</h6>
-                </div>
-                <div className="d-md-flex justify-content-between">
-                    <div className="grid-margin d-flex justify-content-start">
-                        <div className="features-width">
-                            <LazyLoadImage effect="blur" src={Group12} alt="" width="62" height="61" className="img-icons"/>
-                            <h5 className="py-3">Speed<br/>Optimisation</h5>
-                            <p className="text-muted">it is fast as light just one blink and ecery thing is done</p>
-                            <Link to="#">
-                                <p className="readmore-link">Study online</p>
-                            </Link>
-                        </div>
-                    </div>
-                   
-                    
-                    <div className="grid-margin d-flex justify-content-end">
-                        <div className="features-width">
-                            <LazyLoadImage effect="blur" src={Group5} alt="" width="62" height="61" className="img-icons"/>
-                            <h5 className="py-3">Content<br/>Studying</h5>
-                            <p className="text-muted">Lorem ipsum dolor sit amet, tincidunt vestibulum. Fusce egeabus consectetuer turpis, suspendisse.</p>
-                            <Link to="#">
-                                <p className="readmore-link">Readmore</p>
-                            </Link>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <section className="digital-marketing-service" id="digital-marketing-section">
-                <div className="row align-items-center">
-                    <div className="col-12 col-lg-7 grid-margin grid-margin-lg-0 aos-init aos-animate" data-aos="fade-right">
-                        <h3 className="m-0">We Offer Link Between<br/>Students from Senoir One to Six</h3>
-                        <div className="col-lg-7 col-xl-6 p-0">
-                            <p className="py-4 m-0 text-muted">Education is the most powerful weapon which you can use to change the world.</p>
-                            <p className="font-weight-medium text-muted">Here we offer multiple ways of making education effective <Link to="/library"style={{color:"#6c84ff"}}>Happy Studting</Link></p>
-                        </div>
-                    </div>
-                    <div className="col-12 col-lg-5 p-0 img-digital grid-margin grid-margin-lg-0 aos-init aos-animate">
-                        <LazyLoadImage  effect="blur" src={Group1} alt="" width="62" height="61" className="img-fluid"/>
-                    </div>
-                </div>
-           
-            </section>
-          
-           
-      
-        </div>
-    </div>
+   
+       
+    
     </>
     )
 }
