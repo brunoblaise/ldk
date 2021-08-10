dbe1a996bb92a4db5d71b0be40a72eedda5d9101
Mudacumura Brunoblaise <25270994+brunoblaise@users.noreply.github.com>
2021-08-10 10:59:50 +0200

    sumarry

--- src/texteditor/Urt.jsx
+++ src/texteditor/Urt.jsx
@@ -1,42 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import React from 'react'
 import { url } from "../url";
+import useSWR from "swr";
 import { Link } from 'react-router-dom';
 import { format } from "timeago.js";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
 function Urt() {
-    const [message, setMessage] = useState([]);
-  const getProfile = async () => {
-    try {
-  
-      const res = await fetch(`${url}/get/url`, {
-        method: "GET",
-        headers: { jwt_token: localStorage.token }
-      });
-
-      const parseData = await res.json();
-
-      
-
-
-      setMessage(parseData);
-    } catch (err) {
-      console.error(err.message);
-    }
-  };
-  
-  useEffect(() => {
-    getProfile();
-    
-  
-  }, [setMessage]);
+  const { data, error } = useSWR(`${url}/get/url`, fetcher);
+  if (!data) return "I am loading...";
+  if (error) return "there is an error";
 
     return (
         <div>
                    <div className="row mx-0 box1">
                     <div className="">
                         <div className="auth-form-light text-left py-3 px-2 were">
-                            {message.map(chat=>(
-                          <Link to={{pathname:`${chat.url_content}`}} target='_blank' className="were">{format(chat.timestamp)}</Link>
-                            ))}
+                          {data.map(chat=>(
+                          <Link key={chat.url_id} to={{pathname:`${chat.url_content}`}} target='_blank' className="were">{format(chat.timestamp)}</Link>
+                            )) 
+                          }
+                           
                         </div>
                    
                     </div>
