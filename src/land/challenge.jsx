9f81140eda107d8dbfb3c71b1f2f3885c5b4012b
Mudacumura Brunoblaise <brunoblaise94@gmail.com>
2022-03-24 07:23:40 -0700

    let go

--- src/land/challenge.jsx
+++ src/land/challenge.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
 const challenge = ({onQuizStart, data}) => {
- 
+ console.log(data)
   return (
     <div className='cardo'>
       <div className='card-content'>
         <div className='content'>
           <h1>Start the Challenge</h1>
-          {data.length === 0 ? <p>loading questions</p> : <p> Good luck!</p>}
+          {<p> Good luck!</p>}
 
           <button
-            className={data.length === 0 ? 'hide' : 'button is-info is-medium'}
+            className={ 'button is-info is-medium'}
             onClick={onQuizStart}>
             Start
           </button>
